e8997f3fa93bd8b66a59245fabf78b5817aa981d
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-03-28 12:22:57 +0100

    Añadir test para método retirar (debe fallar)

--- src/test/java/dev/anabelen/CuentaTest.java
+++ src/test/java/dev/anabelen/CuentaTest.java
@@ -10,8 +10,14 @@ public class CuentaTest {
         Cuenta cuenta = new Cuenta(1000f,5f);
         cuenta.consignar(500f);
         assertEquals(1500f, cuenta.getSaldo(), 0.01f);
+    }
 
-
+    @Test
+    public void testRetirar() {
+        Cuenta cuenta = new Cuenta(1000f,5f);
+        cuenta.retirar(500f);
+        assertEquals(500f, cuenta.getSaldo(), 0.01f);
+        assertEquals(1, cuenta.getNumeroRetiros());
     }
 
 }
